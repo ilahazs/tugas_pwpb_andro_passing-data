68819eec5e0683ed7bac9974a1ac985d7645f312
ilahazs <ilahazs.weebs@gmail.com>
2021-11-19 22:42:28 +0700

    initial commit

---
+++ app/src/main/java/com/example/latihan_passing_data/MainActivity.java
@@ -0,0 +1,96 @@
+package com.example.latihan_passing_data;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.net.Uri;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
+
+public class MainActivity extends AppCompatActivity implements View.OnClickListener {
+    public Button btnMoveActivity;
+    public Button btnMoveWithDataActivity;
+    public Button btnMoveWithObject;
+    Button btnDialNumber;
+    Button btnMoveResult;
+    TextView tvResult;
+    private int REQUEST_CODE = 100;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        btnMoveActivity = (Button)findViewById(R.id.btn_move_activity);
+        btnMoveActivity.setOnClickListener(this);
+        btnMoveWithDataActivity =
+                (Button)findViewById(R.id.btn_move_with_data_activity);
+        btnMoveWithDataActivity.setOnClickListener(this);
+        btnMoveWithObject =
+                (Button)findViewById(R.id.btn_move_activity_object);
+        btnMoveWithObject.setOnClickListener(this);
+        btnDialNumber = findViewById(R.id.btn_dial_number);
+        btnDialNumber.setOnClickListener(this);
+        btnMoveResult = findViewById(R.id.btn_move_for_result);
+        btnMoveResult.setOnClickListener(this);
+
+        tvResult = findViewById(R.id.tv_result);
+        tvResult.setText(String.format("Hasil : %s", 0));
+    }
+
+    @Override
+    public void onClick(View v) {
+        switch (v.getId()) {
+            case R.id.btn_move_activity:
+                Intent moveIntent = new Intent(MainActivity.this, MoveActivity.class);
+                startActivity(moveIntent);
+                break;
+
+            case R.id.btn_move_with_data_activity:
+                Intent moveWithDataIntent = new Intent(MainActivity.this,
+                        MoveWithDataActivity.class);
+                moveWithDataIntent.putExtra(MoveWithDataActivity.EXTRA_NAME, "Ilham Prabu Zaky S");
+                moveWithDataIntent.putExtra(MoveWithDataActivity.EXTRA_AGE, 17);
+                startActivity(moveWithDataIntent);
+                break;
+
+            case R.id.btn_move_activity_object:
+                Person mPerson = new Person();
+                mPerson.setName("Ilham Prabu Zaky S");
+                mPerson.setAge(17);
+                mPerson.setEmail("ilhamprabuzakys@gmail.com");
+                mPerson.setCity("Kiaracondong, Bandung");
+
+                Intent moveWithObjectIntent = new Intent(MainActivity.this, MoveWithObjectActivity.class);
+                moveWithObjectIntent.putExtra(MoveWithObjectActivity.EXTRA_PERSON, mPerson);
+                startActivity(moveWithObjectIntent);
+                break;
+
+            case R.id.btn_dial_number:
+                String phoneNumber = "085162783743";
+                Intent dialPhoneIntent = new Intent(Intent.ACTION_DIAL, Uri.parse("tel:" + phoneNumber));
+                startActivity(dialPhoneIntent);
+                break;
+
+            case R.id.btn_move_for_result:
+                Intent moveForResultIntent = new Intent(MainActivity.this, MoveForResultActivity.class);
+                startActivityForResult(moveForResultIntent, REQUEST_CODE);
+                break;
+        }
+    }
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+
+        if (requestCode == REQUEST_CODE) {
+            if (resultCode == MoveForResultActivity.RESULT_CODE) {
+                int selectedValue = data.getIntExtra(MoveForResultActivity.EXTRA_SELECTED_VALUE, 0);
+                tvResult.setText(String.format("Hasil : %s", selectedValue));
+            }
+        }
+    }
+}
